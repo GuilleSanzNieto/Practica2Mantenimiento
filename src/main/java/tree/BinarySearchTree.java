ef3a2c8969b37b18b88f683f9188bd41ab8f1f25
martavegas02 <martitavegas286@gmail.com>
2025-03-20 11:50:37 +0100

    subida

--- src/main/java/tree/BinarySearchTree.java
+++ src/main/java/tree/BinarySearchTree.java
@@ -144,7 +144,6 @@ public class BinarySearchTree<T> implements BinarySearchTreeStructure<T> {
             this.left=null; 
             this.right=null; 
         }
-        
     }
 
     @Override

0a24e288d175e0014f05048186585896ecee885b
GuilleSanzNieto <160852982+GuilleSanzNieto@users.noreply.github.com>
2025-03-20 10:11:04 +0100

    binarySearchTree hecho

--- src/main/java/tree/BinarySearchTree.java
+++ src/main/java/tree/BinarySearchTree.java
@@ -1,8 +1,8 @@
 package tree;
 
 import java.util.Comparator;
-import java.util.List;
-import java.util.ArrayList;
+// import java.util.List;
+// import java.util.ArrayList;
 
 public class BinarySearchTree<T> implements BinarySearchTreeStructure<T> {
     private Comparator<T> comparator;
@@ -33,53 +33,163 @@ public class BinarySearchTree<T> implements BinarySearchTreeStructure<T> {
     }
 
     public BinarySearchTree(Comparator<T> comparator) {
+        this.comparator = comparator;
+        this.value = null;
+        this.left = null; 
+        this.right = null; 
+        
         // TODO
     }
 
     @Override
     public void insert(T value) {
+        if (this.value == null){
+            this.value = value;
+        }else{
+            if (comparator.compare(value, this.value) < 0){
+                if (this.left.isLeaf()){
+                    this.left = new BinarySearchTree<T>(comparator);
+                    this.left.value = value;
+                }else{
+                    this.left.insert(value);
+                }
+            }else{
+                if (this.right.isLeaf()){
+                    this.right = new BinarySearchTree<T>(comparator);
+                    this.right.value = value;
+                }else{
+                    this.right.insert(value);
+                }
+            }
+        }
         // TODO
     }
 
     @Override
     public boolean isLeaf() {
         // TODO
-        return false;
+        return this.left == null && this.right == null;
     }
 
     @Override
     public boolean contains(T value) {
+        //Recorro todo el arbol y si encuentro el valor entonces retorno true
+        //si no lo encuentro retorno false
+        boolean cont = false; 
+        if(this.value.equals(value)){
+            cont = true;
+        }else{
+           if(comparator.compare(value, this.value) < 0){
+                if(this.left != null){
+                     cont = this.left.contains(value);
+                }
+            }else if(comparator.compare(value, this.value) > 0){
+                if(this.right != null){
+                    cont = this.right.contains(value);
+                }
+            }
         // TODO
-        return false;
+        }
+        return cont;
+        
     }
-
     @Override
     public T minimum() {
+        if(this.value==null){
+            throw new BinarySearchTreeException("Arbol vacio");
+        }
+        T min=  null;
+        //El minimo estara  a la izquierda del arbol lo mas que se pueda y sera una hoja 
+        if(this.left != null){
+            min = this.left.minimum();
+        }else if(this.left.isLeaf()){
+            min = this.left.value;
+        }
         // TODO
-        return null;
+        return min;
     }
 
     @Override
     public T maximum() {
+        if(this.value==null){
+            throw new BinarySearchTreeException("Arbol vacio");
+        }
+        T max = null;
+        //El maximo estara a la derecha del arbol lo mas que se pueda y sera una hoja
+        if(this.right != null){
+            max = this.right.maximum();
+        }else if(this.right.isLeaf()){
+            max = this.right.value;
+        }
         // TODO
-        return null;
+        return max;
     }
 
     @Override
     public void removeBranch(T value){
-        // TODO
+        if(this.value==null){
+            throw new BinarySearchTreeException("Arbol vacio");
+        }
+       
+        if(comparator.compare(value, this.value) < 0){
+            if(this.left != null){
+                if(this.left.value.equals(value)){
+                    this.left=null; 
+                }else{
+                    this.left.removeBranch(value);
+                }
+            }
+        }else if(comparator.compare(value, this.value) > 0){
+            if(this.right != null){
+                if(this.right.value.equals(value)){
+                    this.right = null; 
+                }else{
+                    this.right.removeBranch(value);
+                }
+            }
+        }else{
+            this.value=null; 
+            this.left=null; 
+            this.right=null; 
+        }
+        
     }
 
     @Override
     public int size() {
+        int cont = 0;
+        //Recorro todo el arbol y por cada nodo que no sea nulo aumento el contador
+        if(this.value != null){
+            cont++;
+            if(this.left != null){
+                cont += this.left.size();
+            }
+            if(this.right != null){
+                cont += this.right.size();
+            }
+        }
         //TODO
-        return 0;
+        return cont;
     }
 
     @Override
     public int depth() {
-        // TODO
-        return 0;
+        int depth = 0; 
+        if(this.value != null){
+            int ileft =0; 
+            int irigth = 0; 
+            if(this.left != null){
+                ileft += this.left.depth();
+            }
+            if(this.right != null){
+                irigth += this.right.depth();
+            }
+            depth = Math.max(ileft, irigth) + 1;
+        }else{
+            depth = 0;
+        }
+                // TODO
+        return depth;
     }
 
     // Complex operations
